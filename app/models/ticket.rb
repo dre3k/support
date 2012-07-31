50bb7930e41eef94cc991b631c8aed57a0f958f2
Andrei Kulakov <krugliy@gmail.com>
2012-07-31 21:51:49 +0300

    ticket annotate

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,21 +1,3 @@
-# == Schema Information
-#
-# Table name: tickets
-#
-#  id         :integer          not null, primary key
-#  no         :string(255)
-#  name       :string(255)      not null
-#  email      :string(255)      not null
-#  dep        :string(255)      not null
-#  subject    :string(255)      not null
-#  message    :text             not null
-#  url        :string(255)
-#  owner_id   :integer
-#  status_id  :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Ticket < ActiveRecord::Base
   # Number format ABC-123456
   NO_ABC    = ('AAA'..'ZZZ').to_a
@@ -52,4 +34,22 @@ class Ticket < ActiveRecord::Base
   end
 
 end
+
+# == Schema Information
 #
+# Table name: tickets
+#
+#  id         :integer          not null, primary key
+#  no         :string(255)
+#  name       :string(255)      not null
+#  email      :string(255)      not null
+#  dep        :string(255)      not null
+#  subject    :string(255)      not null
+#  message    :text             not null
+#  url        :string(255)
+#  owner_id   :integer
+#  status_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
