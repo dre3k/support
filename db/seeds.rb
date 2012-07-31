b17b7f10522e960074da7750eda78b8b2b78f1ef
Andrei Kulakov <krugliy@gmail.com>
2012-07-31 16:06:25 +0300

    make seeding reseeding proof

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,20 @@
-10.times do |i|
-  Customer.create!(name: "Demo_#{i}", email: "demo_#{i}@demo.com")
+begin
+  10.times do |i|
+    Customer.create!(name: "Demo_#{i}", email: "demo_#{i}@demo.com")
+  end
+rescue
+  puts 'Looks like Customers already been seeded'
 end
 
 MEMBER_PASSWORD = 'member'
-10.times do |i|
-  Member.create!(
-    username: "member_#{i}",
-    password: MEMBER_PASSWORD,
-    password_confirmation: MEMBER_PASSWORD
-  )
+begin
+  10.times do |i|
+    Member.create!(
+      username: "member_#{i}",
+      password: MEMBER_PASSWORD,
+      password_confirmation: MEMBER_PASSWORD
+    )
+  end
+rescue
+  puts 'Looks like Members already been seeded'
 end
