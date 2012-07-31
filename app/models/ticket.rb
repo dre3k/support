ab9e7d211cf6eb07d1fdfbf525df5dfe4be88d66
Andrei Kulakov <krugliy@gmail.com>
2012-07-31 17:43:15 +0300

    creating number and url routine for ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,11 +2,26 @@ class Ticket < ActiveRecord::Base
   belongs_to :owner
   belongs_to :status
   attr_accessible :dep, :subject, :message
-  attr_readonly   :no, :dep, :subject, :message, :url
+  attr_readonly   :dep, :subject, :message
 
   validates_presence_of :dep, :subject, :message
 
   has_many :replies, :through => :histories
+
+  after_create do |record|
+    update_attribute :no, Ticket.no_from_id(record.id)
+    update_attribute :url, Ticket.url_from_id(record.id)
+  end
+
+  def self.no_from_id(id)
+    # TODO: format ABC-123456
+    return id
+  end
+
+  def self.url_from_id(id)
+    # TODO: rationalize
+    Digest::SHA1.hexdigest("#{id}-#{UUID.generate}")
+  end
 end
 #
 # == Schema Information
