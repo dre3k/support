ca1af4fecb3ce0eae48c0da2ce8b2a8f74c6c811
Andrei Kulakov <krugliy@gmail.com>
2012-08-02 16:05:58 +0300

    add member reply form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
 
   expose(:current_member) { Member.find(session[:member_id]) if session[:member_id] }
 
+  expose(:all_members_options) do
+    Member.select('id, name').map { |item| [item.name, item.id] }
+  end
+
+  expose(:all_ticketstatuses_options) do
+    TicketStatus.select('id, name').map { |item| [item.name.capitalize, item.id] }
+  end
+
   private
 
   def authorize
